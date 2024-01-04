4d73d47318452631ec07213016e2f8b2a0728386
magedyehiaa <myeof.ii@gmail.com>
2024-01-04 03:02:26 +0200

    log0

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for m in range(10):
     for a in range(m, 10):
-        print("{:d}{:d}".format(m, a),
-              end="\n" if m == 8 and a == 9 else ", ")
+        if m < a:
+            print("{:d}{:d}".format(m, a),
+                  end="\n" if m == 8 and a == 9 else ", ")
