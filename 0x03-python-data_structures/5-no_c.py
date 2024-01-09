6d9de890561572b91449e912c16a77e8b2c9ca67
magedyehiaa <myeof.ii@gmail.com>
2024-01-10 00:48:48 +0200

    log0

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for m in range(len(my_string)):
         if (my_string[m] != 'c' and my_string[m] != 'C'):
             mag += my_string[m]
-            return mag
+    return mag
