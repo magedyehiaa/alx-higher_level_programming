3fa0eef97c9196a7dfe33ea8038583a6e9aa7f2c
magedyehiaa <myeof.ii@gmail.com>
2024-01-10 00:43:44 +0200

    log0

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     mag = ""
     for m in range(len(my_string)):
-        if (my_string[m] != 'c' and my_string[m] != 'c'):
+        if (my_string[m] != 'c' and my_string[m] != 'C'):
             mag += my_string[m]
             return mag
