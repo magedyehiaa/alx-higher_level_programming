7eae21b63d9cd6d429693a6ef60d12834e6ddf30
magedyehiaa <myeof.ii@gmail.com>
2024-01-11 22:13:54 +0200

    log0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,4 +9,3 @@ def best_score(a_dictionary):
             mv = a
             mk = m
     return mk
-
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     return {m: a * 2 for m, a in a_dictionary.items()}
-
